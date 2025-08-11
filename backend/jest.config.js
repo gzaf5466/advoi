3c91a96e0a154b74931503efc8963f0c86030f10
mohd <gzaf5466@gmail.com>
2025-08-11 22:09:05 +0530

    first commit

--- backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
-  verbose: true,
-  // Increase default timeout for potentially slower environments
-  testTimeout: 20000,
-};
